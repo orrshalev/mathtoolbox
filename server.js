b2d37fa6ed34bfd4ce789a8965d740d06da29549
Orr Shalev <shalev.orr@gmail.com>
2021-11-26 23:33:56 -0500

    Yuh

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
-
 const app = express();
+const BodyParser = require("body-parser");
 
 app.use(express.static(__dirname));
+app.use(BodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
 
-
+app.post("/dev.html", function(req,res){
+    console.log(req);
+    console.log(res);
+    console.log("ihfdsg");
+});
 
 app.listen(3000, function(){
-    console.log("my homie jealous");
+    console.log("Server runnin'");
 });
